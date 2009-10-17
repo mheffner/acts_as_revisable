5b324a2be5315c7ff68147a4040a353f8ef0215c
Mike Heffner <mikeh@fesnel.com>
2009-10-17 17:29:57 -0400

    * Allow the :null option to be passed to the migration template. * Set :null=>false for revisable_number and revisable_is_current.

--- generators/revisable_migration/revisable_migration_generator.rb
+++ generators/revisable_migration/revisable_migration_generator.rb
@@ -4,13 +4,13 @@ class RevisableMigrationGenerator < Rails::Generator::NamedBase
       revisable_columns = [
         ["revisable_original_id",      "integer"],
         ["revisable_branched_from_id", "integer"],
-        ["revisable_number",           "integer", 0],
+        ["revisable_number",           "integer", {:default => 0, :null => false}],
         ["revisable_name",             "string"],
         ["revisable_type",             "string"],
         ["revisable_current_at",       "datetime"],
         ["revisable_revised_at",       "datetime"],
         ["revisable_deleted_at",       "datetime"],
-        ["revisable_is_current",       "boolean", 1]
+        ["revisable_is_current",       "boolean", {:default => 1, :null => false}]
       ]
       
       m.migration_template 'migration.rb', 'db/migrate',
--- generators/revisable_migration/templates/migration.rb
+++ generators/revisable_migration/templates/migration.rb
@@ -1,8 +1,9 @@
 <% table_name = class_name.underscore.pluralize -%>
+<% def add_column_opt(opts, opt)  ", :#{opt} => #{opts[opt]}" if opts && opts.key?(opt) end -%>
 class Make<%= class_name.underscore.pluralize.camelize %>Revisable < ActiveRecord::Migration
   def self.up
-    <% cols.each do |column_name,column_type,default| -%>
-    add_column :<%= table_name %>, :<%= column_name %>, :<%= column_type %><%= ", :default => #{default}" unless default.blank? %>
+    <% cols.each do |column_name,column_type,column_opts| -%>
+    add_column :<%= table_name %>, :<%= column_name %>, :<%= column_type %><%= add_column_opt(column_opts, :default) %><%= add_column_opt(column_opts, :null) %>
     <% end -%>
   end
 
