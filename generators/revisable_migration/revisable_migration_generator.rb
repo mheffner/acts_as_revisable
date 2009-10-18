3955f374c18c5b3bbe99e51f830eb38b07f6cf24
Mike Heffner <mikeh@fesnel.com>
2009-10-18 17:04:48 -0400

    * Add indices to the main column used in conditions (orig_id, branch_id,   number, and is_current).

--- generators/revisable_migration/revisable_migration_generator.rb
+++ generators/revisable_migration/revisable_migration_generator.rb
@@ -2,15 +2,15 @@ class RevisableMigrationGenerator < Rails::Generator::NamedBase
   def manifest
     record do |m|
       revisable_columns = [
-        ["revisable_original_id",      "integer"],
-        ["revisable_branched_from_id", "integer"],
-        ["revisable_number",           "integer", {:default => 0, :null => false}],
+        ["revisable_original_id",      "integer", {:index => true}],
+        ["revisable_branched_from_id", "integer",  {:index => true}],
+        ["revisable_number",           "integer", {:default => 0, :null => false, :index => true}],
         ["revisable_name",             "string"],
         ["revisable_type",             "string"],
         ["revisable_current_at",       "datetime"],
         ["revisable_revised_at",       "datetime"],
         ["revisable_deleted_at",       "datetime"],
-        ["revisable_is_current",       "boolean", {:default => 1, :null => false}]
+        ["revisable_is_current",       "boolean", {:default => 1, :null => false, :index => true}]
       ]
       
       m.migration_template 'migration.rb', 'db/migrate',
--- generators/revisable_migration/templates/migration.rb
+++ generators/revisable_migration/templates/migration.rb
@@ -2,14 +2,17 @@
 <% def add_column_opt(opts, opt)  ", :#{opt} => #{opts[opt]}" if opts && opts.key?(opt) end -%>
 class Make<%= class_name.underscore.pluralize.camelize %>Revisable < ActiveRecord::Migration
   def self.up
-    <% cols.each do |column_name,column_type,column_opts| -%>
+<% cols.each do |column_name,column_type,column_opts| -%>
     add_column :<%= table_name %>, :<%= column_name %>, :<%= column_type %><%= add_column_opt(column_opts, :default) %><%= add_column_opt(column_opts, :null) %>
-    <% end -%>
+<% if column_opts && column_opts[:index] -%>
+    add_index :<%= table_name %>, :<%= column_name %>
+<% end -%>
+<% end -%>
   end
 
   def self.down
-    <% cols.each do |column_name,_| -%>
+<% cols.each do |column_name,_| -%>
     remove_column :<%= table_name %>, :<%= column_name %>
-    <% end -%>
+<% end -%>
   end
 end
